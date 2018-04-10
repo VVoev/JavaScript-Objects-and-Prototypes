6a48d41ebbf1631726b4cc1ae6b5457325059556
vladovoev <vladovoev@yahoo.co.uk>
2018-04-10 17:30:14 +0300

    Getters and Setters

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ var cat = {
     color: 'White'
 }
 
-//once set to false cannot be returned
-Object.defineProperty(cat, 'name', { configurable: false });
+Object.defineProperty(cat, 'fullname', {
+    get: function () {
+        return this.name.first + ' ' + this.name.last
+    },
 
-
-Object.defineProperty(cat, 'name', { configurable: true });
+    set: function (value) {
+        var nameparts = value.split(' ');
+        this.name.first = nameparts[0];
+        this.name.last = nameparts[1];
+    }
+})
+display(cat.fullname);
+cat.fullname = "Orange Drag";
+display(cat.fullname);
