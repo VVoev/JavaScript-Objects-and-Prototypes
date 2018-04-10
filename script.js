0fa3a45e06555e569ba2192bea2ca9a188a526b6
vladovoev <vladovoev@yahoo.co.uk>
2018-04-10 19:01:14 +0300

    es6 classes

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 'use strict';
 
-function Animal(voice) {
-    this.voice = voice || 'grrrrrrrrrrrrrrrrr';
-}
-Animal.prototype.speak = function () {
-    display(this.voice);
-}
-function Cat(name, color) {
-    Animal.call(this, "Meow");
-    this.name = name;
-    this.color = color;
+class Animal {
+    constructor(voice) {
+        this.voice = voice || 'grrrr';
+    }
+
+    speak() {
+        display(this.voice);
+    }
 }
 
-Cat.prototype.age = 3;
-
-
-Cat.prototype = Object.create(Animal.prototype);
-Cat.prototype.constructor = Cat
-
-var cat1 = new Cat('Fluffy', 'white');
-var cat2 = new Cat('Muffin', "Brown");
-
-
-Cat.prototype = {
-    age: 5
+class Cat extends Animal {
+    constructor(name, color) {
+        super('Meow');
+        this.name = name;
+        this.color = color;
+    }
 }
 
-var cat3 = new Cat('SnowBell', 'White');
-
-
-display(cat1);
-console.log(cat1);
+var cat = new Cat('fluffly', 'white');
+display(cat);
 
 
 
