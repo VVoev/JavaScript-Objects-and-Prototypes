39447ecc4de7f28f5441ea05b6e02504ff6f3a76
vladovoev <vladovoev@yahoo.co.uk>
2018-04-10 18:56:58 +0300

    prototypal inheritance chain

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 'use strict';
 
+function Animal(voice) {
+    this.voice = voice || 'grrrrrrrrrrrrrrrrr';
+}
+Animal.prototype.speak = function () {
+    display(this.voice);
+}
 function Cat(name, color) {
+    Animal.call(this, "Meow");
     this.name = name;
     this.color = color;
 }
 
 Cat.prototype.age = 3;
+
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat
+
 var cat1 = new Cat('Fluffy', 'white');
 var cat2 = new Cat('Muffin', "Brown");
 
-cat1.age = 21;
 
-display(cat1.age);
-display(cat1.__proto__.age);
+Cat.prototype = {
+    age: 5
+}
 
+var cat3 = new Cat('SnowBell', 'White');
+
+
+display(cat1);
 console.log(cat1);
-console.log(cat2);
+
+
+
+
